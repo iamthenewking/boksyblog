252eb430aee58158ca166bb181d38a24938594e4
jeffrey gaboni <mrgabonijeff@gmail.com>
2020-04-29 21:28:59 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Delog`,
+    title: `Fuck `,
     description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://delog-w3layouts.netlify.com/`,
+    siteUrl: `https://boksyblog.netlify.app/`,
     home: {
-      title: `Hi! I'm Delog`,
+      title: `Hi! I'm Fuck`,
       description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
     },
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
